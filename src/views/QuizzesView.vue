048cd10a32aff434ee07efd50132c15bb7a509d9
Lasha Bulatsashvili <lashareichert@gmail.com>
2022-11-22 17:56:17 +0400

    add favicon and logo

--- src/views/QuizzesView.vue
+++ src/views/QuizzesView.vue
@@ -15,9 +15,8 @@ watch(search, () => {
 });
 
 const beforeEnter = (el) => {
-  console.log("Before enter");
   el.style.opacity = 0;
-  el.style.transform = "translateY(-100px)";
+  el.style.transform = "translateY(-70px)";
 };
 const afterEnter = (el) => {
   el.style.opacity = 1;
@@ -35,32 +34,60 @@ const enter = (el) => {
 <template>
   <div>
     <header>
-      <h1>Quizes</h1>
-      <input v-model.trim="search" type="text" placeholder="Search..." />
+      <h1><img @click="$router.push('/')" src="../assets/Q.png" alt="Logo"></h1>
+      <div>
+        <input v-model.trim="search" type="text" placeholder="Search..." />
+      </div>
     </header>
     <div class="options-container">
-      <TransitionGroup
-        appear
-        @before-enter="beforeEnter"
-        @after-enter="afterEnter"
-        @enter="enter"
-      >
-        <CardComponent
-          v-for="(quiz, index) in quizes"
-          :key="quiz.id"
-          :quiz="quiz"
-          :data-index="(index + 1) / 1.9"
-        />
-      </TransitionGroup>
+
+        <TransitionGroup
+            appear
+            @before-enter="beforeEnter"
+            @after-enter="afterEnter"
+            @enter="enter"
+        >
+          <CardComponent
+              v-for="(quiz, index) in quizes"
+              :key="quiz.id"
+              :quiz="quiz"
+              :data-index="(index + 1) / 1.9"
+          />
+        </TransitionGroup>
+
+      <section v-if="!quizes.length">
+        <div>No Quiz found</div>
+        <div @click="search = '' ">Back to Quizzes</div>
+      </section>
     </div>
   </div>
 </template>
 
 <style scoped>
+header h1 img{
+  width: 75px;
+  height: 75px;
+  margin-left: 5px;
+  cursor: pointer;
+}
+header div{
+  display: flex;
+  justify-content: center;
+}
 .options-container {
   display: flex;
   flex-wrap: wrap;
   margin-top: 40px;
   justify-content: center;
+}
+section{
+  text-align: center;
+  font-size: 30px;
+  margin-top: 50px;
+}
+section div:last-child{
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 16px;
 }
 </style>
